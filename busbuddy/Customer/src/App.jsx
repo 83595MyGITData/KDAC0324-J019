6fe86bd492582bf1970394f56f0d1bcb728f3223
KD3_Swapnil_83595 <swapnildude1717@gmail.com>
2024-08-09 22:14:22 +0530

    Frontend_reservation

--- busbuddy/Customer/src/App.jsx
+++ busbuddy/Customer/src/App.jsx
@@ -11,19 +11,21 @@ import AddBus from './screens/addBus';
 import BusList from './screens/buslist';
 import Reservation from './screens/reservation';
 
+
 function App() {
   return (
     <div>
       {/* <h1 className="page-header" style={{background:"blue",height:50,color:"white",textAlign:"start"}}>BusBuddy
         </h1> */}
       <Routes>
-      <Route path='' element={<Reservation/>}></Route>
+      <Route path='' element={<Login/>}></Route>
       <Route path='/login' element={<Login/>}></Route>
       <Route path='/register' element={<Register/>}></Route>
       <Route path='/customer' element={<Customer/>}></Route>
       <Route path='/home' element={<Home/>}></Route>
       <Route path='/buses' element={<Buses/>}></Route>
-      <Route path='/reservation' element={<Reservation/>}></Route>
+      <Route path='/reservation/:busId' element={<Reservation/>}></Route>
+      
       {/* <Route path='/buslist' element={<BusList/>}></Route> */}
       
       </Routes>
